6685d42454c13b3bb3210569964ffbbb62aab556
Gábor Szolner <Gabor.Szolner@varian.com>
2025-07-13 17:29:52 +0200

    fixes

--- GameStatistic/MapStatEntry.cs
+++ GameStatistic/MapStatEntry.cs
@@ -2,21 +2,33 @@
 
 namespace GameStatistic
 {
-    internal class MapStatEntry(string name, int tWin = 0, int ctWin = 0, int mapStarted = 0, int mapFullPlayed = 0)
+    internal class MapStatEntry
     {
         [JsonPropertyName("name")]
-        public string Name { get; set; } = name;
+        public string Name { get; set; }
 
         [JsonPropertyName("tWin")]
-        public int TtWin { get; set; } = tWin;
+        public int TtWin { get; set; }
 
         [JsonPropertyName("ctWin")]
-        public int CTWin { get; set; } = ctWin;
+        public int CTWin { get; set; }
 
         [JsonPropertyName("mapStarted")]
-        public int MapStarted { get; set; } = mapStarted;
+        public int MapStarted { get; set; }
 
         [JsonPropertyName("mapFullPlayed")]
-        public int MapFullPlayed { get; set; } = mapFullPlayed;
+        public int MapFullPlayed { get; set; }
+
+
+        public MapStatEntry(string name, int tWin = 0, int ctWin = 0, int mapStarted = 0, int mapFullPlayed = 0)
+        {
+            Name = name;
+            TtWin = tWin;
+            CTWin = ctWin;
+            MapStarted = mapStarted;
+            MapFullPlayed = mapFullPlayed;
+        }
+
+        public MapStatEntry() { }
     }
 }
